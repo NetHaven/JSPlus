689611771c9f6c3b4e02a7862f8a7d3e9def6a52
Brian Ferris <NetHaven@gmail.com>
2012-11-16 06:48:32 -0500

    initial project version

---
+++ JSPlus.js
@@ -0,0 +1,30 @@
+var JS;
+
+JS = {};
+
+/******************************************************************************
+* Module: Class Field
+*     
+* Created: 11/16/12
+* 
+* Last Modified: 11/16/12
+*                
+* Package: JS.
+*
+* Extends: None.
+*
+* Events: None.
+*
+* Local Objects: None.
+* 
+* Methods: None.
+*
+* Description: This class represents a field.
+******************************************************************************/
+JS.Field = function(FieldObj)
+{ 															  /* Class Field */
+	if (!FieldObj.name)
+	{
+		throw "Syntax Error: No Field Name Specified";
+	}
+}; 															  /* Class Field */
---
+++ README.txt
@@ -0,0 +1,4 @@
+JSPlus is a library to add more classical object-oriented features to JavaScript.
+
+WHAT'S NEW
+0.1	The creation of a Field object to represent class fields.
