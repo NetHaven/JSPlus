98537753596db81ed997a541f9f99d766c284fea
Brian Ferris <NetHaven@gmail.com>
2012-11-16 22:11:14 -0500

    JSPlus Version 0.2

--- JSPlus.js
+++ JSPlus.js
@@ -17,14 +17,70 @@ JS = {};
 *
 * Local Objects: None.
 * 
-* Methods: None.
+* Methods: getName().
 *
 * Description: This class represents a field.
 ******************************************************************************/
 JS.Field = function(FieldObj)
 { 															  /* Class Field */
+	/* Methods */
+	var getName;
+	
 	if (!FieldObj.name)
 	{
 		throw "Syntax Error: No Field Name Specified";
 	}
+	
+	/**************************************************************************
+	* Module: Method getName
+	*     
+	* Created: 11/17/12
+	* 
+	* Last Modified: 11/17/12
+	*                
+	* Passed Parameters: None.
+	* 
+	* Local Objects: None.
+	* 
+	* Methods Called: None.
+	*
+	* Returned Value: FieldObj.name.
+	* 
+	* Description: Retrieves the name of this field.
+	**************************************************************************/
+	getName = function() 
+	{
+		return (FieldObj.name);	
+	};
+	
+	return ({
+		getName:getName
+	});
 }; 															  /* Class Field */
+
+/******************************************************************************
+* Module: Class Method
+*     
+* Created: 11/17/12
+* 
+* Last Modified: 11/17/12
+*                
+* Package: JS.
+*
+* Extends: None.
+*
+* Events: None.
+*
+* Local Objects: None.
+* 
+* Methods: None.
+*
+* Description: This class represents a method.
+******************************************************************************/
+JS.Method = function(MethodObj)
+{ 															 /* Class Method */
+	if (!MethodObj.name)
+	{
+		throw "Syntax Error: No Method Name Specified";
+	}
+}; 															 /* Class Method */
--- README.txt
+++ README.txt
@@ -1,4 +1,5 @@
 JSPlus is a library to add more classical object-oriented features to JavaScript.
 
 WHAT'S NEW
-0.1	The creation of a Field object to represent class fields.
+0.2	The creation of a Method object to represent class methods.
+0.1	The creation of the top-level JS Package; and also a Field object to represent class fields.
